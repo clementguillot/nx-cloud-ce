084f470f1881825f627ae8a3f5fd1415548963b6
Clément Guillot <clement1.guillot@gmail.com>
2024-02-18 22:00:32 +0100

    Use text instead of variable to help Renovate with Quarkus updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 pluginManagement {
   val quarkusPluginVersion: String by settings
-  val quarkusPluginId: String by settings
   val kotlinVersion: String by settings
   val jnxplusGradlePluginVersion: String by settings
   val spotlessVersion: String by settings
@@ -10,7 +9,7 @@ pluginManagement {
     mavenLocal()
   }
   plugins {
-    id(quarkusPluginId) version quarkusPluginVersion
+    id("io.quarkus") version quarkusPluginVersion
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
